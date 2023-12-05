39af264060311d88ef45372470403c990cbb731a
Alexander Tipanluisa <109613626+AlexHolaMundo@users.noreply.github.com>
2023-12-05 13:15:22 -0500

    add firebase conf sdk

---
+++ assets/js/sdkFirebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyDl1A-dH8W2ngxChfLbYPIug0qKWUZYook",
+    authDomain: "graphical-balm-404517.firebaseapp.com",
+    databaseURL: "https://graphical-balm-404517-default-rtdb.firebaseio.com",
+    projectId: "graphical-balm-404517",
+    storageBucket: "graphical-balm-404517.appspot.com",
+    messagingSenderId: "111611790604",
+    appId: "1:111611790604:web:2780dbcc05b850ebaef529"
+  };
+  const app = initializeApp(firebaseConfig);
+
