0ab798b7b880760e6de5dcfeff3985e20d2fccca
Alexander Tipanluisa <109613626+AlexHolaMundo@users.noreply.github.com>
2023-12-05 13:23:04 -0500

    conection project

--- assets/js/sdkFirebase.js
+++ assets/js/sdkFirebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-  const firebaseConfig = {
+import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+const firebaseConfig = {
     apiKey: "AIzaSyDl1A-dH8W2ngxChfLbYPIug0qKWUZYook",
     authDomain: "graphical-balm-404517.firebaseapp.com",
     databaseURL: "https://graphical-balm-404517-default-rtdb.firebaseio.com",
@@ -9,4 +10,3 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebas
     appId: "1:111611790604:web:2780dbcc05b850ebaef529"
   };
   const app = initializeApp(firebaseConfig);
-
